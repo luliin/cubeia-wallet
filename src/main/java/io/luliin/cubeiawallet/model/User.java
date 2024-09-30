7f3a7b1cc58e76c96bda01c75682e6742ade090b
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2024-09-30 21:40:21 +0200

    Add User model and repo

--- src/main/java/io/luliin/cubeiawallet/model/User.java
+++ src/main/java/io/luliin/cubeiawallet/model/User.java
@@ -1,12 +1,62 @@
 package io.luliin.cubeiawallet.model;
 
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 
 /**
  * @author Julia Wigenstedt
  * Date: 2024-09-30
  */
+@Entity
 
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User(Long id, String email, LocalDateTime createdAt) {
+        this.id = id;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/io/luliin/cubeiawallet/repository/UserRepository.java
+++ src/main/java/io/luliin/cubeiawallet/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package io.luliin.cubeiawallet.repository;
 
+import io.luliin.cubeiawallet.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @author Julia Wigenstedt
  * Date: 2024-09-30
  */
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
